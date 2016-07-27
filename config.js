1d911a1d474e3547d5397dc14e4dad1ee7e8d2cc
Keith Underwood <p4nic510@gmail.com>
2016-07-26 18:16:39 -0600

    link mlab DB

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://admin:admin@ds029725.mlab.com:29725/shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
